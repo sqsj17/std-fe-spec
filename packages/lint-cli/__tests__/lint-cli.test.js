6d4c49671ab2ca699a155a3c3b854ba40aeee0da
hebehealer@163.com <hebeleader@163.com>
2024-04-13 12:55:25 +0800

    feat: update

--- packages/lint-cli/__tests__/lint-cli.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const lintCli = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(lintCli(), 'Hello from lintCli');
-console.info('lintCli tests passed');
